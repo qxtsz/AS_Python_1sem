7790ffbd170faa3d8d4a47a0b4045102786872a9
qxtsz <tema.akhmadullin@yandex.ru>
2025-10-14 15:09:14 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,2 +1,7 @@
 if __name__ == "__main__":
     pass
+#Задание 6 из ЕГЭ-профиль 2026 [https://math-ege.sdamgia.ru/problem?id=77369]
+#Найдите корень уравнения (x-6)^2 = -24x
+for x in range(-100, 100):
+    if (x-6)**2 == -24*x:
+        print(x)
