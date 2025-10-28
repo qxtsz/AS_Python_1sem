0401acb10da802b215191b7093e0845b89e7a202
qxtsz <tema.akhmadullin@yandex.ru>
2025-10-28 14:14:11 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,9 @@
+#Задание 6#
 if __name__ == "__main__":
     pass
+H = int(input())
+A = int(input())
+B = int(input())
+if A < B and A > 0 and B > 0:
+            for i in range(A, B, H):
+                print(i)
