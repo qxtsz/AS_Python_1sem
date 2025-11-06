ccca10e8b7f98eb093bea71a73afc1428d4166f6
qxtsz <tema.akhmadullin@yandex.ru>
2025-11-06 12:23:48 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,11 @@
+#Задание 6#
 if __name__ == "__main__":
     pass
+import math
+n = int(input())
+g = math.cos(1)
+h = math.cos(2)
+j = math.cos(3)
+k = math.cos(n)
+f = g - h + j - n - 1 ** (n + 1 * k)
+print(f)
