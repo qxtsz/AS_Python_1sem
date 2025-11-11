714b1e49be1506e56f8ba6df474ac728d4993c37
qxtsz <tema.akhmadullin@yandex.ru>
2025-11-11 14:15:37 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,21 +1,14 @@
 #задание 6#
 if __name__ == "__main__":
     pass
-import math
 import random
+def zxc(B):
+    maxzxc = B.index(max(B))
+    product = 1
+    for x in B[maxzxc + 1:]:
+        product *= x
+    return product
 q = 5
 B = [random.randint(1, 100) for a in range(q)]
-z = max(B)
 print(B)
-index = B.index(z)
-f = math.prod(B)
-if index == 0:
-    print(f//z)
-if index == 1:
-    print(f // z // B[0])
-if index == 2:
-    print(f // z // B[0] // B[1])
-if index == 3:
-    print(f // z // B[0] // B[1] // B[2])
-if index == 4:
-    print(f // z // B[0] // B[1] // B[2] // B[3])
+print(zxc(B))
