cb5eef0a1b36035d97e9e861f4319230349ad0bd
qxtsz <tema.akhmadullin@yandex.ru>
2025-12-24 08:04:06 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
     pass
+#Задание 6#
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+def NOD(A, B):
+    if A == 0:
+        return B
+    else:
+        return NOD(B % A, A)
+AB = NOD(A, B)
+AC = NOD(A, C)
+AD = NOD(A, D)
+print(AB)
+print(AC)
+print(AD)
