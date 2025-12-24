4feba3c2f5932dc037537ba33219b2dbb25fa3e8
qxtsz <tema.akhmadullin@yandex.ru>
2025-12-24 08:28:41 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
     pass
+#Задание 6#
+import string
+input_file = 'text.txt' 
+output_file = 'newtext.txt' 
+with open(input_file, 'r', encoding='utf-8') as f:
+    text = f.read()
+punctuation_chars = [ch for ch in text if ch in string.punctuation]
+with open(output_file, 'w', encoding='utf-8') as f:
+    f.write(''.join(punctuation_chars))
