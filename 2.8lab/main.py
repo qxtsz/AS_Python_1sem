d341f416d9c6b513bd5f30b2f738c21de6b50779
qxtsz <tema.akhmadullin@yandex.ru>
2025-12-24 08:30:48 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
     pass
+#Задание 6#
+with open('f', 'r', encoding='utf-8') as file:
+    numbers = file.read().split()
+numbers = list(map(int, numbers))
+even_count = sum(1 for num in numbers if num % 2 == 0)
+print(f"Количество чётных чисел: {even_count}")
